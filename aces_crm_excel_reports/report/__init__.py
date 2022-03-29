77a70d101d386fe257b4528fafc986fad6e26f4d
anaconex <62996555+anaconex@users.noreply.github.com>
2022-03-29 23:37:40 +0500

    Transfered to Paid Modules

--- aces_crm_excel_reports/report/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import won_loss_report_xls
-from . import crm_stage_report_xls
--- aces_crm_excel_reports/wizard/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import won_loss_report_wizard
-from . import stages_report_wizard
