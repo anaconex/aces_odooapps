9ab26db60b343e38614ca1efe4113cf118026c16
anaconex <62996555+anaconex@users.noreply.github.com>
2022-03-29 23:38:54 +0500

    Transfered to Paid Git Repo

--- aces_purchase_excel_reports/models/purchase_order_inherit.py
+++
@@ -1,7 +0,0 @@
-# from odoo import models, fields, api
-#
-# class PurchaseOrderInherit(models.Model):
-#     _inherit = 'purchase.order'
-#
-#     grn_date = fields.Datetime(string='GRN Date',  index=True, copy=False)
-#     bill_date = fields.Datetime(string='Bill Date',  index=True, copy=False)
