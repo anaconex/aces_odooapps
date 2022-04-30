1029d4588fca211508040ca809e5a972d39e3a17
anaconex <odoo@anaconex.com>
2022-05-01 03:11:26 +0500

    Attendance Email

---
+++ aces_attendance_email/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "Attendance Email - AnaConEx Solutions",
+    'version': '14.0.2.1.0',
+    'license': 'LGPL-3',
+    'sequence': 1,
+    'category': "Extra Tools",
+    "author": "AnaConEx Solutions LLC",
+    'website': 'https://www.anaconex.com',
+    'summary': """
+        Attendance Email
+    """,
+    'description': '''
+              - This module adds a button to attendance view for sending an email to employees
+    ''',
+    'data': [
+        'views/hr_attendance_view.xml'
+    ],
+    'depends': ['base', 'hr_attendance'],
+    "price": 0,
+    "currency": 'USD',
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+}
